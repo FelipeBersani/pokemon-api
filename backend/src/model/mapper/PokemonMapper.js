f7223a629ef3aa9fbd371602132a4a02b7d7d2f8
Bersani <bersanifelipe@gmail.com>
2020-09-07 23:16:37 -0300

    Ajuste na tratativa de retorno pois o pokemon meltan não possui dados de retorno na api

--- backend/src/model/mapper/PokemonMapper.js
+++ backend/src/model/mapper/PokemonMapper.js
@@ -1,17 +1,18 @@
 module.exports = {
   async fullPokemon(pokemon) {
+    console.log(pokemon.stats)
     return {
       id: pokemon.id,
       name: pokemon.name,
       height: pokemon.height * 10,
       weight: pokemon.weight / 10,
       stats: {
-        hp: pokemon.stats[0].base_stat,
-        attack: pokemon.stats[1].base_stat,
-        defense: pokemon.stats[2].base_stat,
-        special_attack: pokemon.stats[3].base_stat,
-        special_defense: pokemon.stats[4].base_stat,
-        speed: pokemon.stats[5].base_stat,
+        hp: pokemon.stats[0] ? pokemon.stats[0].base_stat : 0,
+        attack: pokemon.stats[1] ? pokemon.stats[1].base_stat : 0,
+        defense: pokemon.stats[2] ? pokemon.stats[2].base_stat : 0,
+        special_attack: pokemon.stats[3] ? pokemon.stats[3].base_stat : 0,
+        special_defense: pokemon.stats[4] ? pokemon.stats[4].base_stat : 0,
+        speed: pokemon.stats[5] ? pokemon.stats[5].base_stat : 0,
       },
       moves: pokemon.moves.map(move => move.move.name),
       types: pokemon.types.map(type => type.type.name),
